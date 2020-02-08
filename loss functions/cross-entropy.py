b42143e6d85f9016352d734e9afb787bf366eaaa
German Abramov <germanjke@gmail.com>
2020-02-08 18:20:08 +0300

    Add files via upload

--- loss functions/cross-entropy.py
+++ loss functions/cross-entropy.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 
-mse_loss = nn.MSELoss()
+ce_loss = nn.CrossEntropyLoss()
 outputs = torch.randn(3, 5, requires_grad=True)
-targets = torch.randn(3, 5)
-loss = mse_loss(outputs,targets)
+targets = torch.tensor([1, 0, 3], dtype=torch.int64)
+loss = ce_loss(outputs, targets)
+
 print(outputs)
 print(targets)
 print(loss)
